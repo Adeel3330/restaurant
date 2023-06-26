58d66a2d0741bae5c15702c473aeb53a7a0da13c
Adeel3330 <adeelansari3330@gmail.com>
2023-06-26 14:14:13 +0500

    register

--- app/Http/Controllers/RestaurantOwnerController.php
+++ app/Http/Controllers/RestaurantOwnerController.php
@@ -19,7 +19,7 @@ class RestaurantOwnerController extends Controller
     public function restaurant_create(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => ['required','email', Rule::unique('restaurants_owner')],
+            'email' => ['required','email', Rule::unique('restaurant_owners')],
             'restaurant_id' => 'required',
             'password' => 'required',
         ]);
