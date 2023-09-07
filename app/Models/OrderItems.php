2a3f5b9f6d31890d9a7b77b66be67f77b38ce569
Adeel3330 <adeelansari3330@gmail.com>
2023-09-07 12:49:18 +0500

    date plus 2

--- app/Models/OrderItems.php
+++ app/Models/OrderItems.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItems extends Model
 {
@@ -20,6 +22,34 @@ class OrderItems extends Model
     public function addon()
     {
         return  $this->hasMany(AddonOrderItems::class, 'order_item_id')->with('addon');
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        // Convert the 'created_at' timestamp to a Carbon instance
+        $createdAt = Carbon::parse($value);
+
+        // Add 2 hours to the 'created_at' timestamp
+        $createdAt->addHours(2);
+
+        // Return the modified timestamp
+        return $createdAt;
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $this->getCreatedAtAttribute($value),
+            set: fn (string $value) => $value,
+        );
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $this->getCreatedAtAttribute($value),
+            set: fn (string $value) => $value,
+        );
     }
 
     protected $fillable = [
--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use App\Models\Users;
 use App\Models\Products;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Orders extends Model
 {
     use HasFactory;
+    protected $dates = ['created_at'];
+
+
     public function user()
     {
         return $this->belongsTo(Users::class);
@@ -19,11 +24,40 @@ class Orders extends Model
         return $this->belongsTo(Restaurants::class);
     }
 
+    public function getCreatedAtAttribute($value)
+    {
+        // Convert the 'created_at' timestamp to a Carbon instance
+        $createdAt = Carbon::parse($value);
+
+        // Add 2 hours to the 'created_at' timestamp
+        $createdAt->addHours(2);
+
+        // Return the modified timestamp
+        return $createdAt;
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $this->getCreatedAtAttribute($value),
+            set: fn (string $value) => $value,
+        );
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $this->getCreatedAtAttribute($value),
+            set: fn (string $value) => $value,
+        );
+    }
+    
     protected $fillable = [
         'user_id',
         'transaction_id',
         'restaurant_id',
         'address',
-        'status'
+        'status',
+ 
     ];
 }
